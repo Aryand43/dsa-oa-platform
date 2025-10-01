f29960cbeef16867fc764ca40288ded0e38fdb13
Aryan Dutt <aryan.dutt43@gmail.com>
2025-10-01 10:49:28 +0800

    Add JSON test cases

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,9 @@
+// Toggle dark mode
+document.getElementById("darkToggle").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+// Fetch leaderboard and inject into table
 window.onload = async () => {
   try {
     const res = await fetch("http://127.0.0.1:8000/leaderboard");
@@ -17,6 +23,13 @@ window.onload = async () => {
 
       tbody.appendChild(row);
     });
+
+    confetti({
+      particleCount: 120,
+      spread: 70,
+      origin: { y: 0.6 }
+    });
+
   } catch (err) {
     console.error("Error fetching leaderboard:", err);
     alert("Could not fetch leaderboard. Is the backend running?");
